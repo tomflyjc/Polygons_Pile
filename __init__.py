69344617604cf19b9bf2990220e7e5c50fa6c0a6
Jean-Christophe Baudin <jeanchristophebaudin@ymail.com>
2019-02-07 14:44:56 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-# (c) JC BAUDIN 2016
+# (c) JC BAUDIN 2019 02 07
 def classFactory(iface):
-  from Polygons_Pile import MainPlugin
+  from .Polygons_Pile_V3 import MainPlugin
   return MainPlugin(iface)
 
